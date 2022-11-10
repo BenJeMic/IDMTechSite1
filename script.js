d03b6841b02f54e4416d75e90230d02e0d1703c2
Fioceena <116049115+Fioceena@users.noreply.github.com>
2022-11-10 03:40:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const swup = new Swup()
+window.addEventListener('load', loadTransition())
+
+function loadTransition() {
+    document.querySelector('body').style.opacity = '1'
+}
+
+function clickFunction(select) {
+    var sel830 = document.getElementById('F830')
+    var sel730 = document.getElementById('F730')
+    var sel630 = document.getElementById('F630')
+
+    document.getElementById('b830').className = 'button'
+    document.getElementById('b730').className = 'button'
+    document.getElementById('b630').className = 'button'
+
+    if (select === 'F830') {
+        sel830.style.opacity = '1'
+        sel730.style.opacity = '0'
+        sel630.style.opacity = '0'
+        document.getElementById('b830').className = 'button active'
+    } else if (select === 'F730') {
+        sel830.style.opacity = '0'
+        sel730.style.opacity = '1'
+        sel630.style.opacity = '0'
+        document.getElementById('b730').className = 'button active'
+    } else {
+        sel830.style.opacity = '0'
+        sel730.style.opacity = '0'
+        sel630.style.opacity = '1'
+        document.getElementById('b630').className = 'button active'
+    }
+}
+
+
+const body = document.querySelector('#htmlhtml');
+
+const tl = new TimelineMax();
+
+tl.fromTo(body, 1.2, { transform: 'translateX(-100%)', opacity: '0' }, { transform: 'translateX(0%)', opacity: '1', ease: Power2.easeOut }, "+=1")
